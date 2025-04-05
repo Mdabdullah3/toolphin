8a110fd4bee2da90dc9ca4e0828733b17d0cd9da
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2025-04-05 20:56:28 +0600

    added two packages

---
+++ src/empty/isEmpty.ts
@@ -0,0 +1,8 @@
+export const isEmpty = (value: any): boolean => {
+  return (
+    value == null ||
+    (typeof value === "string" && value.trim() === "") ||
+    (Array.isArray(value) && value.length === 0) ||
+    (typeof value === "object" && Object.keys(value).length === 0)
+  );
+};
